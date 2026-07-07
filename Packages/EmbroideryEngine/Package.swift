700d0e5162be8886ac340ad81b74d7ab37b26f06
Sebastian Steinacher <sebastian.steinacher@proton.me>
2026-07-07 20:56:48 +0200

    Add EmbroideryEngine Swift package with Swift Testing placeholder test

---
+++ Packages/EmbroideryEngine/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "EmbroideryEngine",
+    platforms: [
+        .iOS(.v17),
+        .macOS(.v14),
+    ],
+    products: [
+        .library(name: "EmbroideryEngine", targets: ["EmbroideryEngine"]),
+    ],
+    targets: [
+        .target(name: "EmbroideryEngine"),
+        .testTarget(name: "EmbroideryEngineTests", dependencies: ["EmbroideryEngine"]),
+    ],
+    swiftLanguageModes: [.v6]
+)
